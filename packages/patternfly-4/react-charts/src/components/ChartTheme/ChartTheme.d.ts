83e689ed292e697eda3ce3d2de14375d53a1fe12
Jon Jackson <jonjacks@redhat.com>
2019-05-22 13:18:13 -0400

    fix(Charts): Fix typescript declarations for ChartTheme related components
    Fixes #2061

--- packages/patternfly-4/react-charts/src/components/ChartTheme/ChartTheme.d.ts
+++ packages/patternfly-4/react-charts/src/components/ChartTheme/ChartTheme.d.ts
@@ -1,15 +1,22 @@
-export const ChartBaseTheme = any;
-export const ChartDonutTheme = any;
+import { VictoryThemeInterface } from "victory";
 
-export interface ChartThemeColor {
+interface ChartBaseThemeInterface extends VictoryThemeInterface {};
+interface ChartDonutThemeInterface extends VictoryThemeInterface {};
+
+interface ChartThemeColorInterface {
   blue: string;
   default: string;
   green: string;
   multi: string;
-};
+}
 
-export interface ChartThemeVariant {
+interface ChartThemeVariantInterface {
   dark: string;
   default: string;
   light: string;
-};
+}
+
+export declare const ChartThemeColor: ChartThemeColorInterface;
+export declare const ChartThemeVariant: ChartThemeVariantInterface;
+export declare const ChartBaseTheme: ChartBaseThemeInterface;
+export declare const ChartDonutTheme: ChartDonutThemeInterface;
